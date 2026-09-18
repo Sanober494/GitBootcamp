b76fa35d4246b8b6ba88c87435dc2dc8d5d7d66d
Sanober Sarfaraz Ahmed <75628824+Sanober494@users.noreply.github.com>
2026-09-18 12:52:28 -0500

    Add my_title function for title casing text

--- title_case_v1.py
+++ title_case_v1.py
@@ -3,3 +3,9 @@
 # your own version is equally fine.
 def my_cap(text):
 return text[0].upper() + text[1:]
+
+def my_title(text):
+words = []
+for t in text.split():
+words.append(my_cap(t))
+return " ".join(words)
